09646b94df446c2ed79de3a791cc3f7d55adb581
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2024-11-22 16:34:46 +0100

    systemctl

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-apt-get update && yes | apt-get install -y git sudo vim libnet-ssleay-perl libio-socket-ssl-perl curl && cd && git clone https://github.com/thibaultduponchelle/pause.git -b tib && cd pause/bootstrap && ./selfconfig-root --host pause.example.com --user=USER --pass=PASS --self-signed
+apt-get update && yes | apt-get install -y git sudo vim libnet-ssleay-perl libio-socket-ssl-perl curl systemctl && cd && git clone https://github.com/thibaultduponchelle/pause.git -b tib && cd pause/bootstrap && ./selfconfig-root --host pause.example.com --user=USER --pass=PASS --self-signed
 
 
