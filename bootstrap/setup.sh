cf77c4cb68eba1521aef8e1e26175d9a6c6f7410
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2024-11-22 15:53:59 +0100

    Yes

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-apt-get update && apt-get install git sudo vim && cd && git clone https://github.com/thibaultduponchelle/pause.git -b tib && cd pause/bootstrap && ./selfconfig-root --host pause.example.com --user=USER --pass=PASS --self-signed
+apt-get update && yes | apt-get install -y git sudo vim && cd && git clone https://github.com/thibaultduponchelle/pause.git -b tib && cd pause/bootstrap && ./selfconfig-root --host pause.example.com --user=USER --pass=PASS --self-signed
 
 
