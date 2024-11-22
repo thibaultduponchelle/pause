3064cb6952d0c31064e0ba821ea40b9eac97bb04
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2024-11-22 15:51:58 +0100

    Fix

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-apt-get update && apt-get install git && cd && git clone https://github.com/thibaultduponchelle/pause.git -b tib && cd pause/bootstrap
-./selfconfig-root --host pause.example.com --user=USER --pass=PASS --self-signed
+apt-get update && apt-get install git sudo vim && cd && git clone https://github.com/thibaultduponchelle/pause.git -b tib && cd pause/bootstrap && ./selfconfig-root --host pause.example.com --user=USER --pass=PASS --self-signed
 
 
