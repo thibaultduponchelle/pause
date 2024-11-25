375478e0e12064ce5e256b071521df99cd959e99
Thibault Duponchelle <thibault.duponchelle@gmail.com>
2024-11-25 07:38:55 +0100

    Update setup.sh

--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -3,3 +3,6 @@
 apt-get update && yes | apt-get install -y git sudo vim libnet-ssleay-perl libio-socket-ssl-perl curl systemctl && cd && git clone https://github.com/thibaultduponchelle/pause.git -b tib && cd pause/bootstrap && ./selfconfig-root --host pause.example.com --user=USER --pass=PASS --self-signed --repo-user=thibaultduponchelle --repo-branch=tib
 
 next PLINE if $pkg =~ /^Local$/;
+
+select package, version, dist, distname, file from packages;
+
